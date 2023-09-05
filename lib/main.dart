1684dea9d344dcdf98159f9b9e1a05f4a879c9c3
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-09-04 22:15:16 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Projeto parte 1")),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
