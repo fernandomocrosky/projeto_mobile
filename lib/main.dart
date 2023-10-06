0f651279ae1721a9511d36ec5cb3f5390f10390b
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-10-06 18:17:09 -0300

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_mobile/components/my_app.dart';
+import "package:firebase_core/firebase_core.dart";
+import "firebase_options.dart";
 
 main() {
   runApp(MyApp());
