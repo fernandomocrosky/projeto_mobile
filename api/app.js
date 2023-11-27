6c0e87224cca57610ff485ba2af8f60d35b701dc
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-11-27 11:56:42 -0300

    Post success

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const mongoose = require("mongoose");
 mongoose
-    .connect("mongodb+srv://fernandobm:636322159a@cluster0.amktk8o.mongodb.net/fictions?retryWrites=true&w=majority")
+    .connect("mongodb+srv://fernandobm:<P>@cluster0.amktk8o.mongodb.net/fictions?retryWrites=true&w=majority")
     .then(() => app.emit("pronto"))
     .catch((e) => console.log(e))
 ;
